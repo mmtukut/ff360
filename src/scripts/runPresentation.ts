ff6fd6f0e28ce8ee2a74f054ba9f16c1f5a1b609
mmtukut <57846066+mmtukut@users.noreply.github.com>
2024-12-10 11:32:11 +0100

    Delete src directory

--- src/scripts/runPresentation.ts
+++
@@ -1,9 +0,0 @@
-import { PresentationBuilder } from '../presentation/presentation';
-
-async function runPresentation() {
-    const builder = new PresentationBuilder();
-    await builder.buildPresentation();
-    console.log('Presentation ready!');
-}
-
-runPresentation().catch(console.error); 
